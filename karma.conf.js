416be777645e7bad618afb4fdf83c0b233e27ae9
a-koptsov <a-koptsov@users.noreply.github.com>
2014-08-12 16:34:51 +0400

    Changed test files structure

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'test/test.js'
+            'test/**/*.js'
         ],
 
         // list of files to exclude
@@ -24,7 +24,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'test/test.js': ['browserify']
+            'test/**/*.js': ['browserify']
         },
 
         // test results reporter to use
