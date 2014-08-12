a697f442c76666d6fd15b0603a87ba0a0b8d60c9
a-koptsov <a-koptsov@users.noreply.github.com>
2014-08-12 11:35:40 +0400

    Added PhantomJS launcher for Karma

--- karma.conf.js
+++ karma.conf.js
@@ -45,11 +45,11 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+        singleRun: true,
 
         browserify: {
             watch: true
