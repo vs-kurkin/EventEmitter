3520ccc0a500dfd398ed05d655a295600f979091
Vladislav Kurkin <b-vladi@yandex-team.ru>
2014-08-29 18:12:54 +0400

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var EventEmitter = require('../EventEmitter');
 
-describe('Check exports', function() {
-    it('EventEmitter', function() {
+describe('Экспорт API', function () {
+    it('EventEmitter', function () {
         return expect(typeof EventEmitter).toBe('function');
     });
 
@@ -16,8 +16,72 @@ describe('Check exports', function() {
     it('EventEmitter.EVENT_REMOVE_LISTENER', function () {
         return expect(EventEmitter.EVENT_REMOVE_LISTENER).toBe('removeListener');
     });
+});
 
-    it('EventEmitter.current', function () {
-        return expect(EventEmitter.EVENT_REMOVE_LISTENER).toBeDefined();
+describe('Остановка выполнения обработчиков события', function () {
+    it('Остановка выполнения текущего события', function () {
+        var emitter = new EventEmitter();
+        var emitter2 = new EventEmitter();
+        var r;
+
+        emitter
+            .on('event', function () {
+                expect(EventEmitter.stopEmit()).toBe(true);
+                expect(this.stopEmit()).toBe(true);
+                expect(this.stopEmit('event')).toBe(true);
+
+                expect(emitter2.stopEmit()).toBe(false);
+                expect(emitter2.stopEmit('event')).toBe(false);
+
+                r = true;
+            })
+            .on('event', function () {
+                r = false;
+            })
+            .emit('event');
+
+        expect(r).toBe(true);
+    });
+
+    it('Остановка выполнения другого события', function () {
+        var emitter = new EventEmitter();
+        var r;
+
+        emitter
+            .on('event', function () {
+                expect(EventEmitter.stopEmit('some')).toBe(false);
+                expect(this.stopEmit('some')).toBe(false);
+
+                r = true;
+            })
+            .on('event', function () {
+                r = false;
+            })
+            .emit('event');
+
+        expect(r).toBe(false);
+    });
+
+    it('Остановка текущего стека выполнения', function () {
+        var emitter = new EventEmitter();
+        var r = '';
+
+        emitter
+            .on('ready', function () {
+                EventEmitter.stopEmit();
+                this.emit('event');
+            })
+            .on('event', function () {
+                r += 'a';
+            })
+            .on('event', function () {
+                r += 'b';
+            })
+            .on('ready', function () {
+                r += 'c';
+            })
+            .emit('ready');
+
+        return expect(r).toBe('ab');
     });
 });
